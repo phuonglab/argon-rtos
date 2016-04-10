e37925458ac8591b7de1557180e4b078cf2ef300
Chris Reed <flit@me.com>
2016-04-10 15:11:10 -0500

    Moved board specifics to board.h/cpp.

--- apps/lowrider/src/main.cpp
+++ apps/lowrider/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Chris Reed
+ * Copyright (c) 2015-2016 Chris Reed
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -28,6 +28,7 @@
  */
 
 #include "argon/argon.h"
+#include "board.h"
 #include "audio_output.h"
 #include "audio_output_converter.h"
 #include "audio_filter.h"
@@ -509,63 +510,6 @@ void init_fs()
 
     printf("Sequence #1: %s\n", g_firstSequence->channels[1]);
     g_kickSeq.set_sequence(g_firstSequence->channels[1]);
-}
-
-void init_board()
-{
-    CLOCK_EnableClock(kCLOCK_PortA);
-    CLOCK_EnableClock(kCLOCK_PortB);
-    CLOCK_EnableClock(kCLOCK_PortC);
-    CLOCK_EnableClock(kCLOCK_PortD);
-
-    // I2C0 pins
-    const port_pin_config_t pinConfig = {
-        .pullSelect = kPORT_PullDisable,
-        .slewRate = kPORT_FastSlewRate,
-        .passiveFilterEnable = kPORT_PassiveFilterDisable,
-        .openDrainEnable = kPORT_OpenDrainEnable,
-        .driveStrength = kPORT_LowDriveStrength,
-        .mux = kPORT_MuxAlt2,
-    };
-    PORT_SetMultiplePinsConfig(PORTB, (1 << 3)|(1 << 2), &pinConfig);
-
-    // SAI pins
-    PORT_SetPinMux(PORTC, 8, kPORT_MuxAlt4);
-    PORT_SetPinMux(PORTA, 5, kPORT_MuxAlt6);
-    PORT_SetPinMux(PORTA, 12, kPORT_MuxAlt6);
-    PORT_SetPinMux(PORTA, 13, kPORT_MuxAlt6);
-    PORT_SetPinMux(PORTC, 5, kPORT_MuxAlt4);
-
-    // LED pins
-    PORT_SetPinMux(PORTA, 1, kPORT_MuxAsGpio);
-    PORT_SetPinMux(PORTA, 2, kPORT_MuxAsGpio);
-    PORT_SetPinMux(PORTD, 5, kPORT_MuxAsGpio);
-
-    const gpio_pin_config_t gpioOut = {
-        .pinDirection = kGPIO_DigitalOutput,
-        .outputLogic = 0,
-    };
-    GPIO_PinInit(GPIOA, 1, &gpioOut);
-    GPIO_PinInit(GPIOA, 2, &gpioOut);
-    GPIO_PinInit(GPIOD, 5, &gpioOut);
-
-    // SPI0 pins for SD card
-    // PTC4 = SPI0_PCS0
-    // PTD1 = SPI0_SCK
-    // PTD2 = SPI0_SOUT
-    // PTD3 = SPI0_SIN
-    // PTB16 = SD_CARD_DETECT
-    PORT_SetPinMux(PORTC, 4, kPORT_MuxAlt2);
-    PORT_SetPinMux(PORTD, 1, kPORT_MuxAlt2);
-    PORT_SetPinMux(PORTD, 2, kPORT_MuxAlt2);
-    PORT_SetPinMux(PORTD, 3, kPORT_MuxAlt2);
-    PORT_SetPinMux(PORTB, 16, kPORT_MuxAsGpio);
-
-    const gpio_pin_config_t gpioIn = {
-        .pinDirection = kGPIO_DigitalInput,
-        .outputLogic = 0,
-    };
-    GPIO_PinInit(GPIOB, 16, &gpioIn);
 }
 
 int main(void)
